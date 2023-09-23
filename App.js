fb97f87d5e7baf9749dc1013d29a71b6f40c8810
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-23 23:47:20 +0300

    Todo app with React Native initial

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home screen</Text>
+    </View>
+  )
+}
+
+const LoginScreen = () => {
+  return(
+    <View>
+      <Text>Login screen</Text>
+    </View>
+  )
+}
+
+const storeData = async(value) => {
+  try{
+    await AsyncStorage.setItem('my-key', value);
+  }catch(e){
+    console.log(e)
+  }
+}
+
+const getData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('my-key');
+    if (value !== null) {
+      console.log(value)
+      return value
+    }
+  } catch (e) {
+    console.log(e)
+  }
+};
+
 
 export default function App() {
+  // storeData("Hello from localStorage")
+  const valueFromLocalStorage = getData()
+  console.log(valueFromLocalStorage)
+  console.log("Hello again123")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
