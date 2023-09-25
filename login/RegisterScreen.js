f850527e3f3133249f14d83c0b2a7a3a2833deae
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-25 12:59:47 +0300

    Keyboard handling

--- login/RegisterScreen.js
+++ login/RegisterScreen.js
@@ -1,11 +1,53 @@
-import { View, Text, TextInput, StyleSheet, Button } from "react-native"
+import React, {useState, useEffect} from 'react'
+import { View, Text, TextInput, StyleSheet, Button, ScrollView } from "react-native"
 import { commonStyles, loginScreenStyles } from "../utils/styles";
 import { LinearGradient } from 'expo-linear-gradient';
+import { isStrongPassword } from '../utils/passwordValidation';
 
-const RegisterScreen = () => {
+const RegisterScreen = ({ navigation }) => {
+  const [firstname, setFirstname] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState({})
+  const [isFormValid, setIsFormValid] = useState(false)
+
+  useEffect(() => {
+    validateForm();
+  }, [firstname, lastname, email, password])
+
+  const validateForm = () => {
+    let errors = {}
+    if (!firstname){
+      errors.firstname = "Firstname is required"
+    }
+
+    if(!email){
+      errors.email = 'Email is required.'
+    }else if(!/\S+@\S+\.\S+/.test(email)){
+      errors.email = 'Email is invalid'
+    }
+
+    if(!password){
+      errors.password = 'password is required'
+    }else if(!isStrongPassword(password)){
+      errors.password = 'password must contain atleast 6 letters, one uppercase, one lowercase letter, a digit and an special character'
+    }
+
+    setErrors(errors)
+    setIsFormValid(Object.keys(errors).length === 0)
+  }
+
+  const handleSubmit = () => {
+    if(isFormValid){
+      console.log("Form submitted succesfully!")
+    }else{
+      console.log("Form has errors. Please correct them.")
+    }
+  }
   return (
     <LinearGradient style={{ flex: 1 }} colors={['#833ab4', '#fd1d1d', '#fcb045']} >
-      <View style={loginScreenStyles.loginScreenBase}>
+      <View style={styles.loginScreenBase}>
         <View style={{
           flex: 1,
           justifyContent: "flex-end",
@@ -21,32 +63,37 @@ const RegisterScreen = () => {
           flex: 4,
           alignItems: "center",
         }}>
-          <TextInput style={loginScreenStyles.input}
+          <TextInput style={styles.registesInput}
             underlineColorAndroid="transparent"
             placeholder="First name"
             autoCapitalize="none"
           />
-          <TextInput style={loginScreenStyles.input}
+          <TextInput style={styles.registesInput}
             underlineColorAndroid="transparent"
             placeholder="Last name"
             autoCapitalize="none"
           />
-          <TextInput style={loginScreenStyles.input}
+          <TextInput style={styles.registesInput}
             underlineColorAndroid="transparent"
             placeholder="Email"
             autoCapitalize="none"
           />
-          <TextInput style={loginScreenStyles.input}
+          <TextInput style={styles.registesInput}
             underlineColorAndroid="transparent"
             placeholder="Password"
             autoCapitalize="none"
             secureTextEntry={true}
           />
           <Button title="Register" />
+          {Object.values(errors).map((error, index) => (
+            <Text key={index} style={commonStyles.errorText}>
+              {error}
+            </Text>
+          ))}
           <Text>
             Already have an account?{" "}
             <Text style={{ color: 'blue' }}
-              onPress={() => { }}>
+              onPress={() => navigation.navigate("Login")}>
               Log in
             </Text>
           </Text>
@@ -56,4 +103,29 @@ const RegisterScreen = () => {
   )
 }
 
+
+
 export default RegisterScreen
+const styles = StyleSheet.create({
+  registesInput: {
+    height: 40,
+    width: 150,
+    margin: 15,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: '#7a42f4',
+    borderWidth: 1
+  },
+  loginScreenBase:{
+    flex: 1,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 30,
+    borderWidth: 10,
+    borderRadius: 30,
+    backgroundColor: 'rgba(255,255,255,0.3)'
+  },
+
+}
+)
