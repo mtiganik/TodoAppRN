2b846aef11430d10a00c92d466d213e860d625f5
Mihkel Tiganik <mtiganik@gmail.com>
2023-09-26 15:17:23 +0300

    Test login

---
+++ login/LoginTesting.js
@@ -0,0 +1,64 @@
+import { View, Button, Text } from "react-native"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+input2= {
+    "email": "aaa@bbb.ee",
+    "password": "q1W",
+    "firstName": "aaa",
+    "lastName": "aaa"
+}
+
+
+
+const inputInitials = {
+  firstName: "dsadsa",
+  lastName: "bbb",
+  email: "aa1@bb.ee",
+  password:"Q1w2E#",
+  confirmPassword: ""
+}
+
+
+const url = "https://taltech.akaver.com/api/v1/Account/Register"
+
+
+const LoginTest = ({navigation}) => {
+
+  const[input, setInput] = useState(inputInitials)
+  const[error, setError] = useState("")
+  const[message, setMessage] = useState("")
+
+  const handlePress = async () => {
+    try{
+      const response = await axios.post(url, {
+        email: input.email,
+        password: input.password,
+        firstName: input.firstName,
+        lastName: input.lastName
+      });
+      console.log("after post")
+
+      const {token} = response.data
+      console.log(token)
+
+      await AsyncStorage.setItem('token', token);
+      navigation.navigate('Home');
+    }catch (error){
+      console.log("Error occured")
+      const msg = error.response.data.messages
+      setError("Error" + msg)
+    }
+
+  }
+  return(
+    <View>
+      <Button title="Press me" onPress={handlePress}/>
+      <Text>{error}</Text>
+    </View>
+
+  )
+}
+
+export default LoginTest
